4ef22f82fb35f49dfd980236985a3965636071d2
Paul T. <45326403+katistix@users.noreply.github.com>
2023-12-14 08:52:51 +0000

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,24 @@
 import './App.css';
+import { useState } from 'react'; // Adaucam functia pt state
+
 
 function App() {
+  // un state nou pentru contor
+  const [contor, setContor] = useState(0); // initial = 0
+
+  function cresteValoarea(){
+    setContor(contor+1);
+  }
+  function scadeValoarea(){
+    setContor(contor-1);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
+    <div>
+      {/* Afisam valoarea contor in pagina */}
+      <h1>{contor}</h1>
+      <button onClick={cresteValoarea}>Creste</button>
+      <button onClick={scadeValoarea}> Scade</button>
     </div>
   );
 }
